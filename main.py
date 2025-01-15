8336f1ea3757b069553ed8c459372cd2ad2b3869
Danle33 <pokava2005@gmail.com>
2025-01-16 00:05:12 +0100

    Began implementing available moves

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ for i in range(8):
 #colors
 WHITE = (255, 255, 255)
 BLACKY = (30, 30, 30)
+DARK_GREEN_TRANSPARENT = (0, 50, 0, 50)
 
 #globals
 minutes = 0.25
@@ -31,8 +32,9 @@ availableIncrements = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 30, 60]
 SQUARE_SIZE = WIDTH / 8
 game_started = False
 
-names_white = ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
-names_black = [piece.lower() for piece in names_white]
+image_dot = pygame.image.load("Assets/shared/dot.png")
+image_dot = pygame.transform.scale(image_dot, (SQUARE_SIZE * 0.7, SQUARE_SIZE * 0.7))
+rect_dot = image_dot.get_rect()
 
 # FUNCTIONS
 
@@ -44,17 +46,16 @@ def render_text(s, x, y, font_size):
     rect_text.center = (x,y)
     screen.blit(text_surface, rect_text)
 
-# caluclates screen position of a piece based on matrix position
-def calc_position(row, column):
-    x = TABLE_X + column * SQUARE_SIZE + SQUARE_SIZE / 2
-    y = TABLE_Y + row * SQUARE_SIZE + SQUARE_SIZE / 2
-    return (int(x), int(y))
-
 # calculates new dimensions based on initial ones which are 450*975
 # handles rescalling while keeping aspect ratio
 def f(x):
     return x * WIDTH / 450
 
+# SETUP
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Offline Chess")
+
+clock = pygame.time.Clock()
 
 # CLASSES
 class Slider(pygame.sprite.Sprite):
@@ -116,56 +117,117 @@ class Slider(pygame.sprite.Sprite):
             increment = availableIncrements[i]
 
 class Piece(pygame.sprite.Sprite):
-    def __init__(self, name, image, x, y, row, column):
+    def __init__(self, name, image, row, column):
         super().__init__()
-        self.x = x
-        self.y = y
         self.name = name
-        self.image = pygame.transform.scale(image, (SQUARE_SIZE * 0.7, SQUARE_SIZE * 0.7))
-        self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
 
+        self.image = pygame.transform.scale(image, (int(SQUARE_SIZE * 0.7), int(SQUARE_SIZE * 0.7)))  # Scale image
         self.row = row
         self.column = column
 
-        self.init_available_squares()
+        # Rect for the image (used for rendering)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.calc_position_screen(self.row, self.column)
+
+        # Separate rect for the larger square
+        self.rect_square = pygame.Rect(0, 0, SQUARE_SIZE, SQUARE_SIZE)
+        self.rect_square.center = self.rect.center  # Align centers
+
+        self.update_available_squares()
 
         self.dragging = False
-        self.clicked = False  # To track simple clicks
+        self.holding = False  # To track simple clicks
+        self.selected = False
+    
+    # caluclates screen position of a piece based on matrix position
+    def calc_position_screen(self, row, column):
+        x = TABLE_X + column * SQUARE_SIZE + SQUARE_SIZE / 2
+        y = TABLE_Y + row * SQUARE_SIZE + SQUARE_SIZE / 2
+        return (int(x), int(y))
+    
+    # calculates row and column for a given screen position
+    # inverse function of the calc_position_screen()
+    def calc_position_matrix(self, center):
+        (x, y) = center
+        row = (y - TABLE_Y) / SQUARE_SIZE
+        column = (x -TABLE_X) / SQUARE_SIZE
+        return (int(row), int(column))
     
     # initializes pairs of (row, column) coordinates of available squares
-    def init_available_squares(self):
-        pass
+    def update_available_squares(self):
+        self.available_squares = []
+
+        if self.name == 'P':
+            # square in front of the pawn
+            if TABLE_MATRIX[self.row - 1][self.column] == '.':
+                self.available_squares.append((self.row - 1, self.column))
+
+            # two squares from starting position
+            if self.row == 6 and TABLE_MATRIX[self.row - 2][self.column] == '.':
+                self.available_squares.append((self.row - 2, self.column))
+           
     
     def display_available_squares(self):
         # and also mark selected square
-        pass
+        marked_square = pygame.Surface((SQUARE_SIZE, SQUARE_SIZE), pygame.SRCALPHA)
+        marked_square.fill(DARK_GREEN_TRANSPARENT)
+        
+        rect_marked_square = marked_square.get_rect()
+        (x, y) = self.calc_position_screen(self.row, self.column)
+        rect_marked_square.center = (x + 1, y + 1) # adding +1 cause board image doesnt contain perfect squares and alignment
+
+        screen.blit(marked_square, rect_marked_square)
+
+        for (row, column) in self.available_squares:
+            (x, y) = self.calc_position_screen(row, column)
+            rect_dot.center = (x, y)
+            screen.blit(image_dot, rect_dot)
+    
+    def legal_move(self):
+        return False
+
 
     def handle_event(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            # Check if the mouse clicked on the sprite
-            if self.rect.collidepoint(event.pos):
-                self.dragging = True
-                self.clicked = True  # Initially assume it's a click
+        (mouse_x, mouse_y) = pygame.mouse.get_pos()
+        mouse_on_piece = self.rect_square.collidepoint((mouse_x, mouse_y))
 
-        elif event.type == pygame.MOUSEBUTTONUP:
-            # Handle release
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if mouse_on_piece:
+                self.selected = not self.selected
+                self.holding = True
+            else:
+                self.selected = False
+        
+        if event.type == pygame.MOUSEBUTTONUP and mouse_on_piece:
             if self.dragging:
                 self.dragging = False
-                # If the mouse hasnt moved, treat it as a click
-                if self.clicked:
-                    self.display_available_squares()
-        
-        elif event.type == pygame.MOUSEMOTION:
-            # Handle dragging
-            if self.dragging:
-                # handle board bounds
-                curr_x = max(event.pos[0], 0 + self.rect.size[0] / 2)
-                curr_x = min(curr_x, WIDTH - self.rect.size[0] / 2)
-                curr_y = max(event.pos[1], TABLE_Y + self.rect.size[1] / 2)
-                curr_y = min(curr_y, TABLE_Y + 8 * SQUARE_SIZE - self.rect.size[1] / 2)
-                self.rect.center = (curr_x, curr_y)  # Update position
-                self.clicked = False  # Moving means its not a simple click
+                self.selected = False
+                # Align the piece to the closest square if move is legal
+                if self.legal_move():
+                    self.row, self.column = self.calc_position_matrix(self.rect_square.center)
+                    self.rect.center = self.calc_position_screen(self.row, self.column)
+                    self.rect_square.center = self.rect.center
+
+                    # Update available squares after the move
+                    self.update_available_squares()
+
+                else: # else reset the piece to current position
+                    self.rect.center = self.calc_position_screen(self.row, self.column)
+                    self.rect_square.center = self.rect.center
+            self.holding = False
+
+        if event.type == pygame.MOUSEMOTION and mouse_on_piece and self.selected and self.holding:
+            self.dragging = True
+
+        if self.dragging and self.selected:
+            # Update the position of the piece while dragging
+            curr_x = max(mouse_x, self.rect.size[0] / 2)
+            curr_x = min(curr_x, WIDTH - self.rect.size[0] / 2)
+            curr_y = max(mouse_y, TABLE_Y + self.rect.size[1] / 2)
+            curr_y = min(curr_y, TABLE_Y + 8 * SQUARE_SIZE - self.rect.size[1] / 2)
+
+            self.rect.center = (curr_x, curr_y)
+            self.rect_square.center = self.rect.center
 
 
 # background image
@@ -178,29 +240,25 @@ image_table = pygame.transform.scale(image_table, (WIDTH, WIDTH))
 rect_table = image_table.get_rect()
 rect_table.topleft = (TABLE_X, TABLE_Y)
 
-# SETUP
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Offline Chess")
-
-clock = pygame.time.Clock()
-
 # home screen
 slider1 = Slider(f(200))
 slider2 = Slider(f(400))
 
-route_whites = "Assets/dark/pieces white/"
-route_blacks = "Assets/dark/pieces black/"
+# assuming player is white
+route_player = "Assets/dark/pieces white/"
+route_opponent = "Assets/dark/pieces black/"
 
-image_K = pygame.image.load(route_whites + "K.png")
+image_K = pygame.image.load(route_player + "K.png")
 image_K = pygame.transform.scale(image_K, (f(100), f(100)))
 rect_K = image_K.get_rect()
 rect_K.center = (WIDTH / 2 - f(100), HEIGHT - f(100))
 
-image_k = pygame.image.load(route_blacks + "k.png")
+image_k = pygame.image.load(route_opponent + "k.png")
 image_k = pygame.transform.scale(image_k, (f(100), f(100)))
 rect_k = image_k.get_rect()
 rect_k.center = (WIDTH / 2 + f(100), HEIGHT - f(100))
 
+# home screen loop
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -240,55 +298,60 @@ while 1:
     pygame.display.flip()
     clock.tick(60)
 
-if player_color == "b":
-    # invert king and queen and also routes
-    names_white = ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R']
-    names_black = [piece.lower() for piece in names_white]
-    (route_whites, route_blacks) = (route_blacks, route_whites)
+# SETTING PIECES UP
+pieces_player = pygame.sprite.Group()
+pieces_opponent = pygame.sprite.Group()
 
-# whites
-pieces_white = pygame.sprite.Group()
+# assuming player is white
+names_player = ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
+names_opponent = [piece.lower() for piece in names_player]
+
+# if player is indeed black, switch player route to blacks and change piece names to lowercase, also replace king and queen
+if player_color == "b":
+    names_player = ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'r', 'n', 'b', 'k', 'q', 'b', 'n', 'r']
+    names_opponent = [piece.upper() for piece in names_player]
+    (route_player, route_opponent) = (route_opponent, route_player)
+
 row = 6
 column = 0
-for name in names_white:
+for name in names_player:
     if column == 8:
         column = 0
-    if name != 'P':
+    if name != names_player[0]: # done with pawns
         row = 7
-    (x, y) = calc_position(row, column)
-    pieces_white.add(Piece(name, pygame.image.load(route_whites + f"{name}.png"), x, y, row, column))
+    pieces_player.add(Piece(name, pygame.image.load(route_player + f"{name}.png"), row, column))
     TABLE_MATRIX[row][column] = name
     column += 1
 
-# blacks
-pieces_black = pygame.sprite.Group()
 row = 1
 column = 0
-for name in names_white:
+for name in names_opponent:
     if column == 8:
         column = 0
-    if name != 'P':
+    if name != names_opponent[0]: # done with pawns
         row = 0
-    (x, y) = calc_position(row, column)
-    pieces_black.add(Piece(name, pygame.image.load(route_blacks + f"{name}.png"), x, y, row, column))
+    pieces_opponent.add(Piece(name, pygame.image.load(route_opponent + f"{name}.png"), row, column))
     TABLE_MATRIX[row][column] = name
     column += 1
 
-# game
-# "white" pieces are always the ones on the bottom of the screen, even though their color is black
+# game loop
 while 1:
+    screen.blit(image_bg, (0, 0))
+    screen.blit(image_table, rect_table)
+
+    for piece in pieces_player:
+        if piece.selected:
+            piece.display_available_squares()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        for piece_white in pieces_white:
-            piece_white.handle_event(event)
+        for piece in pieces_player:
+            piece.handle_event(event)
 
-    screen.blit(image_bg, (0, 0))
-    screen.blit(image_table, rect_table)
-
-    pieces_white.draw(screen)
-    pieces_black.draw(screen)
+    pieces_player.draw(screen)
+    pieces_opponent.draw(screen)
 
     pygame.display.flip()
     clock.tick(60)
